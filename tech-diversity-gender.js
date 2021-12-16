c1cd832f3a1ea7b61d0695c6d1cf15af3b893b28
Meriem allali <86774346+meriemallali@users.noreply.github.com>
2021-12-16 21:55:38 +0100

    Update tech-diversity-gender.js

--- tech-diversity-gender.js
+++ tech-diversity-gender.js
@@ -7,61 +7,43 @@ function TechDiversityGender() {
   // characters.
   this.id = 'tech-diversity-gender';
 
-  // Layout object to store all common plot layout parameters and
-  // methods.
-  this.layout = {
-    // Locations of margin positions. Left and bottom have double margin
-    // size due to axis and tick labels.
-    leftMargin: 130,
-    rightMargin: width,
-    topMargin: 30,
-    bottomMargin: height,
-    pad: 5,
-
-    plotWidth: function() {
-      return this.rightMargin - this.leftMargin;
-    },
-
-    // Boolean to enable/disable background grid.
-    grid: true,
-
-    // Number of axis tick labels to draw so that they are not drawn on
-    // top of one another.
-    numXTickLabels: 10,
-    numYTickLabels: 8,
-  };
-
-  // Middle of the plot: for 50% line.
-  this.midX = (this.layout.plotWidth() / 2) + this.layout.leftMargin;
-
-  // Default visualisation colours.
-  this.femaleColour = color(255, 0 ,0);
-  this.maleColour = color(0, 255, 0);
-
-  // Property to represent whether data has been loaded.
-  this.loaded = false;
-
   // Preload the data. This function is called automatically by the
   // gallery when a visualisation is added.
   this.preload = function() {
     var self = this;
     this.data = loadTable(
-      './data/tech-diversity/gender-2018.csv', 'csv', 'header',
+      './data/tech-diversity/gender.csv', 'csv', 'header',
       // Callback function to set the value
       // this.loaded to true.
       function(table) {
         self.loaded = true;
-      });
+      }); 
+    };
 
-  };
+    this.setup = function() {
+      if (!this.loaded) {
+        console.log('Data not yet loaded');
+        return;
+      }
+  
+      // Create a select DOM element.
+      this.select = createSelect();
+      this.select.position(400, 80);
+  
+      // Fill the options with all company names.
+      var companies = this.data.columns;
+      // First entry is empty.
+      for (let i = 1; i < companies.length; i++) {
+        this.select.option(companies[i]);
+      }
+    };
 
-  this.setup = function() {
-    // Font defaults.
-    textSize(16);
-  };
+    this.destroy = function() {
+      this.select.remove();
+    };
 
-  this.destroy = function() {
-  };
+  // Create a new pie chart object.
+  this.pie = new PieChart(width / 2, height / 2, width * 0.4);
 
   this.draw = function() {
     if (!this.loaded) {
@@ -69,80 +51,26 @@ function TechDiversityGender() {
       return;
     }
 
-    // Draw Female/Male labels at the top of the plot.
-    this.drawCategoryLabels();
+    // Get the value of the company we're interested in from the
+    // select item.
+    var company = this.select.value();
 
-    var lineHeight = (height - this.layout.topMargin) /
-        this.data.getRowCount();
+    // Get the column of raw data for questionType.
+    var col = this.data.getColumn(company);
 
-    for (var i = 0; i < this.data.getRowCount(); i++) {
+    // Convert all data strings to numbers.
+    col = stringsToNumbers(col);
 
-      // Calculate the y position for each company.
-      var lineY = (lineHeight * i) + this.layout.topMargin;
+    // Copy the column labels from the table (the first item of each row).
+    var labels = this.data.getColumn(0);
 
-      // Create an object that stores data from the current row.
-      var company = {
-        // Convert strings to numbers.
-        'name': this.data.getString(i, 'company'),
-        'female': this.data.getNum(i, 'female'),
-        'male': this.data.getNum(i, 'male'),
-      };
+    // Colour to use for each category.
+    var colours = ['#84db84','green'];
+   
+    // Make a title.
+    var title = 'Gender diversity at ' + company;
 
-      // Draw the company name in the left margin.
-      fill(0);
-      noStroke();
-      textAlign('right', 'top');
-      text(company.name,
-           this.layout.leftMargin - this.layout.pad,
-           lineY);
-
-      // Draw female employees rectangle.
-      fill(this.femaleColour);
-      rect(this.layout.leftMargin,
-           lineY,
-           this.mapPercentToWidth(company.female),
-           lineHeight - this.layout.pad);
-
-      // Draw male employees rectangle.
-      fill(this.maleColour);
-      rect(this.layout.leftMargin + this.mapPercentToWidth(company.female),
-           lineY,
-           this.mapPercentToWidth(company.male),
-           lineHeight - this.layout.pad);
-    }
-
-    // Draw 50% line
-    stroke(150);
-    strokeWeight(1);
-    line(this.midX,
-         this.layout.topMargin,
-         this.midX,
-         this.layout.bottomMargin);
-
-  };
-
-  this.drawCategoryLabels = function() {
-    fill(0);
-    noStroke();
-    textAlign('left', 'top');
-    text('Female',
-         this.layout.leftMargin,
-         this.layout.pad);
-    textAlign('center', 'top');
-    text('50%',
-         this.midX,
-         this.layout.pad);
-    textAlign('right', 'top');
-    text('Male',
-         this.layout.rightMargin,
-         this.layout.pad);
-  };
-
-  this.mapPercentToWidth = function(percent) {
-    return map(percent,
-               0,
-               100,
-               0,
-               this.layout.plotWidth());
+    // Draw the pie chart!
+    this.pie.draw(col,labels, colours, title);
   };
 }
