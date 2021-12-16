6c478aac69227cf5ab74c60854eeec6b8bcf6b7c
Meriem allali <86774346+meriemallali@users.noreply.github.com>
2021-12-16 16:02:09 +0100

    Update tech-diversity-race.js

--- tech-diversity-race.js
+++ tech-diversity-race.js
@@ -1,63 +1,29 @@
-const colors = ['red', 'green', 'blue', 'pink','yellow','purple'];
+const colors = ['#654b75', '#a3544b', '#4a8a53', 'pink','#6c8394','#949621'];  
 const thickness = 40;
-//segments = {};
-
-// col : [565, 54487, 4548]
-// this.dat.twichya : ['woman', 'male' 'nes ness']
-
-
-/* let segments = {
-  foo: 34,
-  bar: 55,
-  baz: 89
-};
- */
 let radius = 150, centerX, centerY;
 let labelSpace = 30;
 
 function makeLegendItem(keys, i, colors) {
 
 
-  var x = centerX + 100 + radius;
-  var y = centerY + (labelSpace * i) - radius / 3;
-  var boxWidth = labelSpace/2
-  // var boxWidth = labelSpace / 2;
+  var x = centerX + 80 + radius;
+  var y = centerY + (labelSpace * i) - radius /3 -100;
+  var boxWidth = labelSpace/2;
   var boxHeight = labelSpace/2;
-  // var boxHeight = labelSpace / 2;
 
   fill(colors);
   stroke(0);
   strokeWeight(1);
-
   rect(x, y, boxWidth, boxHeight);
 
   fill('black');
   noStroke();
   textAlign('left', 'center');
   textSize(12);
-  text(keys, x + boxWidth + 20, y +boxWidth/2);
-  // nofill()
+  text(keys, x + boxWidth + 10, y + boxWidth/2);
 };
 
-// function mySetup() {
-  
-//   /* // createCanvas(windowWidth, windowHeight);
-//   noFill();
-//   strokeWeight(thickness);
-//   strokeCap(SQUARE);
-//   ellipseMode(RADIUS);
-//   textAlign(CENTER, CENTER);
-//   textSize(20);
-
-//   centerX = width / 2;
-//   centerY = height / 2; */
-// }
-
 function myDraw() {
-  // console.log('this.data :', this.data);
-
-  // console.log('segments :', segments);
-
   let keys = Object.keys(segments);
   let total = keys.map(k => segments[k]).reduce((v, s) => v + s, 0);
   let start = 0;
@@ -77,37 +43,38 @@ function myDraw() {
     mouseAngle += TWO_PI;
   }
 
+    // Create a new doghnut chart .
   for (let i = 0; i < keys.length; i++) {
     noFill();
     stroke(colors[i]);
-    strokeWeight(60);
-
-    //noFill();
+    strokeWeight(80);
     let angle = segments[keys[i]] / total * TWO_PI;
-
-
-   arc(centerX, centerY, radius, radius, start + 100, start + angle);
+    arc(centerX, centerY, radius, radius, start + 100, start + angle);
 
     // Check mouse pos
-/*     if (mouseDist > radius - thickness / 2 &&
-      mouseDist < radius + thickness / 2) {
+    var d = mouseDist > radius - thickness ; 
+    var ds =  mouseDist < radius + thickness ;
 
-      if (mouseAngle > start && mouseAngle < start + angle) {
-        // If the mouse is the correct distance from the center to be hovering over
-        // our "donut" and the angle to the mouse cursor is in the range for the
-        // current slice, display the slice information
-        push();
-        noStroke();
-        fill(colors[i]);
-        text(`${keys[i]}: ${segments[keys[i]]}`, centerX, centerY);
-        pop();
+    // console.log(ds)
+
+    if (d  && ds ) {
+      if (mouseAngle > start && mouseAngle < start + angle +100) {
+    // If the mouse is the correct distance from the center to be hovering over
+    // our "donut" and the angsle to the mouse cursor is in the range for the
+    // current slice, display the slice information
+
+      push();
+      noStroke();
+      fill(colors[i]);
+      textSize(20);
+      text(segments[keys[i]]  ,centerX,centerY);
+      //text(`${keys[i]}: ${segments[keys[i]]}`, centerX, centerY);
+      pop();
       }
-    } */
-    if(keys){
-      this.makeLegendItem(keys[i], i, colors[i]);
-    }
- 
-
+  } 
+  if(keys){
+    this.makeLegendItem(keys[i], i, colors[i]);
+  }
     start += angle;
   }
 }
@@ -115,8 +82,6 @@ function myDraw() {
 
 
 function TechDiversityRace() {
-
-
   // Name for the visualisation to appear in the menu bar.
   this.name = 'Tech Diversity: Race';
 
@@ -130,7 +95,6 @@ function TechDiversityRace() {
   // Preload the data. This function is called automatically by the
   // gallery when a visualisation is added.
   this.preload = function() {
-    // debugger;
     var self = this;
     this.data = loadTable(
       './data/tech-diversity/race-2018.csv', 'csv', 'header',
@@ -138,7 +102,6 @@ function TechDiversityRace() {
       // this.loaded to true.
       function(table) {
         self.loaded = true;
-        // debugger
       });
   };
 
@@ -147,10 +110,9 @@ function TechDiversityRace() {
       console.log('Data not yet loaded');
       return;
     }
-
     // Create a select DOM element.
     this.select = createSelect();
-    this.select.position(350, 40);
+    this.select.position(400, 80); //350 , 40 
 
     // Fill the options with all company names.
     var companies = this.data.columns;
@@ -158,8 +120,6 @@ function TechDiversityRace() {
     for (let i = 1; i < companies.length; i++) {
       this.select.option(companies[i]);
     }
-
-    // createCanvas(windowWidth, windowHeight);
     noFill();
     strokeWeight(thickness);
     strokeCap(SQUARE);
@@ -175,18 +135,16 @@ function TechDiversityRace() {
     this.select.remove();
   };
 
-  // Create a new pie chart object.
-  // this.pie = new PieChart(width / 2, height / 2, width * 0.4);
-
-  
-
-
   this.draw = function() {
     var that = this;
+
     if (!this.loaded) {
       console.log('Data not yet loaded');
       return;
     }
+    
+    var x = width /2; 
+    var y = height/2;
 
     // Get the value of the company we're interested in from the
     // select item.
@@ -194,39 +152,24 @@ function TechDiversityRace() {
 
     // Get the column of raw data for companyName.
     var col = this.data.getColumn(companyName);
-
-    // debugger
-
     // Convert all data strings to numbers.
     col = stringsToNumbers(col);
 
     // Copy the row labels from the table (the first item of each row).
     var labels = this.data.getColumn(0);
-
-    // Colour to use for each category.
-    var colours = ['blue', 'red', 'green', 'pink', 'purple', 'yellow'];
-
     // Make a title.
     var title = 'Employee diversity at ' + companyName;
 
-    // console.log('this.data :', this.data);
-
+    strokeWeight(2)
+    textAlign('center', 'center');
+    textSize(24);
+    text(title, x, y -250);
     // format data for doughnut input
-
     segments = {};
-    
     col.forEach((value, index) => {
-      // labels = ["name, name"]
-      // col = [44, 454, 457]
       segments[labels[index]] = value;
     });
 
-    // console.log('segments :', segments);
     myDraw();
-    // this.pie.draw(col, labels, colours, title);
-
-    
-    
-
   };
 }
