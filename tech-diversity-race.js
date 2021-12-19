3504d47fbc50a0e17e10fddf9ff23a747ad61c3b
Code-Curious <thedragamer@gmail.com>
2021-12-19 22:24:18 +0100

    Fix tech diversity race hover

--- tech-diversity-race.js
+++ tech-diversity-race.js
@@ -3,6 +3,27 @@ const thickness = 40;
 let radius = 150, centerX, centerY;
 let labelSpace = 30;
 
+// draw an arrow for a vector at a given base position (to debug mouse pointer hovering)
+function drawArrow(base, vec, myColor) {
+  push();
+  stroke(myColor);
+  strokeWeight(3);
+  fill(myColor);
+  translate(base.x, base.y);
+  line(0, 0, vec.x, vec.y);
+  rotate(vec.heading());
+  let arrowSize = 7;
+  translate(vec.mag() - arrowSize, 0);
+  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+  pop();
+}
+
+function formatNumber(value) {
+  return parseFloat(parseFloat(value).toFixed(2));
+}
+
+
+
 function TechDiversityRace() {
   // Name for the visualisation to appear in the menu bar.
   this.name = 'Tech Diversity: Race';
@@ -104,47 +125,73 @@ function TechDiversityRace() {
     // return an array of strings of companies name 
     let keys = Object.keys(segments);
     let total = keys.map(k => segments[k]).reduce((v, s) => v + s, 0);
-    let start = 0;
-    // Check the mouse distance and angle
+    let startAngle = 0;
+    
+    // Check the mouse distance and currentSliceEndAngle
     let mouseDist = dist(centerX, centerY, mouseX, mouseY);
-    // the angle between a vector pointing to the right, and the vector
+    let centerVector = createVector(centerX, centerY);
+    let fromCenterToMousePointerVector = createVector(mouseX - centerX, mouseY - centerY);
+    let rightAxisVector = createVector(centerX - radius, 289 - centerY); // DONT KNOW WHERE THIS 289 COMES FROM HONESTLY  ¯\_(ツ)_/¯
+
+    // the currentSliceEndAngle between a vector pointing to the right, and the vector
     // pointing from the center of the window to the current mouse position.
-    let mouseAngle =
-      createVector(1, 0).angleBetween(
-        createVector(mouseX - centerX, mouseY - centerY)
-      );
+    let mouseAngle = rightAxisVector.angleBetween(fromCenterToMousePointerVector);
+
     // Counter angles 
     if (mouseAngle < 0) {
       mouseAngle += TWO_PI;
     }
+    
+    // FOR DEBUGGING : 
+    /* let angleInDegs = degrees(mouseAngle).toFixed(2);
+    ellipse(centerVector.x, centerVector.y, 1, 1)
+    text( formatNumber(angleInDegs) + " degrees", 200, 50);
+    text( formatNumber(mouseAngle) + " radians", 200, 150);
+    drawArrow(centerVector, fromCenterToMousePointerVector, 'gold');
+    drawArrow(centerVector, rightAxisVector, 'cyan'); */
 
     // Create a new doghnut chart .
     for (let i = 0; i < keys.length; i++) {
-    noFill();
-    stroke(colors[i]);
-    strokeWeight(80);
-    let angle = segments[keys[i]] / total * TWO_PI;
-    arc(centerX, centerY, radius, radius, start + 100, start + angle);
-
-    // if the mouse is inside doghnut 
-    // display the percentage in the center of doghnut 
+      noFill();
+      stroke(colors[i]);
+      strokeWeight(80);
+      let currentSliceStartAngle = startAngle;
+      if (currentSliceStartAngle < 0 ) {
+        currentSliceStartAngle += TWO_PI;
+      }
+     
+      let currentSliceAngleIncrement = segments[keys[i]] / total * TWO_PI;
+      // if (currentSliceAngleIncrement < 0 ) {
+      //   currentSliceAngleIncrement += TWO_PI;
+      // }
+      let currentSliceEndAngle = currentSliceStartAngle + currentSliceAngleIncrement;
       
-    if (mouseDist > radius - thickness/2  && mouseDist < radius + thickness/2) {
-      if (mouseAngle > start && mouseAngle < start + angle ) {
+      // if (currentSliceEndAngle < 0 ) {
+      //   currentSliceEndAngle += TWO_PI;
+      // }
+      
+      arc(centerX, centerY, radius, radius, currentSliceStartAngle/*  + 100 */, currentSliceEndAngle);
+      
+      // if the mouse is inside doghnut 
+      // display the percentage in the center of doghnut 
+        
+      // "White" race is the first value to draw (purple)
+      let isCursorDistanceOnDoughnut =  (mouseDist > radius - thickness) && (mouseDist < radius + thickness);
+      let isCursorAngleOnCurrentSlice = (mouseAngle > currentSliceStartAngle) && (mouseAngle < currentSliceEndAngle);
+      if (isCursorDistanceOnDoughnut && isCursorAngleOnCurrentSlice) {
         push()
         noStroke();
         textSize(30);
         fill(colors[i]);
-        text(segments[keys[i]] + "%",centerX-20,centerY); 
-        pop()       
+        text(formatNumber(segments[keys[i]]) + "%", centerX-20, centerY); 
+        pop();
       }
-     }
-     
+      
 
-     if(keys){
-        this.makeLegendItem(keys[i], i, colors[i]);
-      }
-      start += angle;
+      if(keys){
+          this.makeLegendItem(keys[i], i, colors[i]);
+        }
+        startAngle += currentSliceAngleIncrement;
     }
     pop(); // restore last drawing config.
   };
