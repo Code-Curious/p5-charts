d229fafbf13eb5666ce6840cd76a6a016ea531ef
Meriem allali <86774346+meriemallali@users.noreply.github.com>
2021-12-19 19:05:41 +0100

    Add files via upload

---
+++ world-population-projections.js
@@ -0,0 +1,34 @@
+function Worldpopulation() {
+
+  // Name for the visualisation to appear in the menu bar.
+  this.name = 'World population projections: 2021-2031';
+
+  // Each visualisation must have a unique ID with no special
+  // characters.
+  this.id = 'population-projections';
+
+  // Title to display above the plot.
+  this.title = 'World Population Projections 2021-2031';
+
+  // Preload the data. This function is called automatically by the
+  // gallery when a visualisation is added.
+  this.preload = function() {
+    var self = this;
+    this.data = loadTable(
+      './data/tech-diversity/worldPopulationProjections.csv', 'csv', 'header',
+      // Callback function to set the value
+      // this.loaded to true.
+      function(table) {
+        self.loaded = true;
+      }); 
+
+  };
+  this.setup = function()
+  {
+    background();
+  }
+  this.draw = function(){
+
+  }
+  
+}
