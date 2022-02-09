650fe36078247676aecec2012cace313664f4da6
Code-Curious <thedragamer@gmail.com>
2022-02-09 19:49:42 +0100

    Random bubble colors

--- bubble.js
+++ bubble.js
@@ -1,54 +1,55 @@
 function Bubble(_name, size)
 {
-this.size = size;
-// this.target_size = 20;
-this.pos = createVector(0,0,0);
-// this.x = width/2;
-// this.y = height/2;
-this.direction = createVector(0,0);
-this.name = _name;
-this.color = color(random(0,255), random(0,255), random(0,255));
-// this.data = [];
+    this.size = size;
+    // this.target_size = 20;
+    this.pos = createVector(0,0,0);
+    // this.x = width/2;
+    // this.y = height/2;
+    this.direction = createVector(0,0);
+    this.name = _name;
+    // this.color = color(random(0,255), random(0,255), random(0,255));
+    this.color = randomColor({ luminosity: 'light' });
+    // this.data = [];
 
-let ySpeed = random(-0.5,0,5);
-let xSpeed = -0.5;
+    let ySpeed = random(-0.5,0,5);
+    let xSpeed = -0.5;
 
-var rad = 60; 
+    var rad = 60; 
 
-this.draw = function()
-{
-    push();
-    // var x = this.pos.x;
-    // var y = this.pos.y;
-    
-    noStroke();
-    fill(this.color);
-    ellipse(this.pos.x, this.pos.y, this.size);
-    fill(0);
-    textAlign(CENTER,CENTER);
-    textStyle(BOLDITALIC);
-    textSize(15);
-    text(this.name,this.pos.x ,this.pos.y);
-    // this.pos.y = this.pos.y + ySpeed;
-    // this.pos.x = this.pos.x + ySpeed;
+    this.draw = function()
+    {
+        push();
+        // var x = this.pos.x;
+        // var y = this.pos.y;
+        
+        noStroke();
+        fill(this.color);
+        ellipse(this.pos.x, this.pos.y, this.size);
+        fill(0);
+        textAlign(CENTER,CENTER);
+        textStyle(BOLDITALIC);
+        textSize(15);
+        text(this.name,this.pos.x ,this.pos.y);
+        // this.pos.y = this.pos.y + ySpeed;
+        // this.pos.x = this.pos.x + ySpeed;
 
 
-    // moving it around
-    /*  this.pos.x++
-    this.pos.y++
+        // moving it around
+        /*  this.pos.x++
+        this.pos.y++
 
-    //bounce off left and right
-    if(this.pos.x < this.pos || this.pos.x > width) {
-        xSpeed = xSpeed * -1;
-    }
+        //bounce off left and right
+        if(this.pos.x < this.pos || this.pos.x > width) {
+            xSpeed = xSpeed * -1;
+        }
 
-    // bounce off top and bottom
-    if(this.pos.y < this.pos || this.pos.y > height) {
-        ySpeed = ySpeed * -1;
-    } */
-            
+        // bounce off top and bottom
+        if(this.pos.y < this.pos || this.pos.y > height) {
+            ySpeed = ySpeed * -1;
+        } */
+                
 
-    
+        
         pop();
 }
 
