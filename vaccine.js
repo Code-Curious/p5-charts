e6bc9c96e2998ec311299b26714164b664db4381
Code-Curious <thedragamer@gmail.com>
2022-02-27 13:32:18 +0100

    add booster dose

--- vaccine.js
+++ vaccine.js
@@ -1,6 +1,6 @@
 function Vaccine(){
     // Name for the visualisation to appear in the menu bar.
-  this.name = 'Vaccine percentages per country';
+  this.name = 'COVID vaccine percentages per country 2022';
 
   // Each visualisation must have a unique ID with no special
   // characters.
@@ -10,7 +10,7 @@ function Vaccine(){
   this.loaded = false;
 
   //make a title for the chart 
-  this.title = 'Vaccine percentages per country as of chi le3ba';
+  this.title = 'COVID-19 vaccine doses percentage at ';
 
   // Preload the data. This function is called automatically by the
   // gallery when a visualisation is added.
@@ -42,23 +42,27 @@ function Vaccine(){
     for (let i = 0; i < countries.length; i++) {
       this.select.option(countries[i]);
     }
+    console.log('this.select :', this.select);
     
     this.select.changed(x => {
-      let selectedCountry = this.select.value();
-      this.relevantRow = this.data.getRows().find(element => element.get(0).trim().toLowerCase() == selectedCountry.trim().toLowerCase());
+      this.selectedCountry = this.select.value();
+      this.relevantRow = this.data.getRows().find(element => element.get(0).trim().toLowerCase() == this.selectedCountry.trim().toLowerCase());
 
       // console.profile('data-prep')
-      // let row = this.csvRows.find(element => element.get(0).trim().toLowerCase() == selectedCountry.trim().toLowerCase());
+      // let row = this.csvRows.find(element => element.get(0).trim().toLowerCase() == this.selectedCountry.trim().toLowerCase());
       let firstDosePercentage = parseInt(this.relevantRow.get(1).trim().substring(0, this.relevantRow.get(1).trim().length - 1));
       let remainingPercentageFromFirstDose = 100 - firstDosePercentage;
       let secondDosePercentage = parseInt(this.relevantRow.get(2).trim().substring(0, this.relevantRow.get(2).trim().length - 1));
       let remainingPercentageFromSecondDose = 100 - secondDosePercentage;
-
-      var waffleNames = ["First dose", "Second dose" ];
-
+      let boosterDosePercentage = parseInt(this.relevantRow.get(3).trim().substring(0, this.relevantRow.get(3).trim().length - 1));
+      let remainingPercentageFromBoosterDose = 100 - boosterDosePercentage;
+      
+      var waffleNames = ["First dose", "Second dose", "Booster dose"];
+      
       var valuesNames = ['Remaining Population', 'Vaccinated'];
-
-      let wafflesData = [[remainingPercentageFromFirstDose, firstDosePercentage],[remainingPercentageFromSecondDose, secondDosePercentage]]; 
+      
+      let wafflesData = [[remainingPercentageFromFirstDose, firstDosePercentage],[remainingPercentageFromSecondDose, secondDosePercentage], [remainingPercentageFromBoosterDose, boosterDosePercentage]]; 
+      console.log('wafflesData :', wafflesData);
 
       // console.profileEnd('data-prep')
       for (var i = 0; i < wafflesData.length; i++) {
@@ -66,6 +70,10 @@ function Vaccine(){
         this.waffles.push(new Waffle(x, 200, 200, 200, 10, 10, wafflesData[i], waffleNames[i], valuesNames));
       }
     })
+
+    this.select.elt.selectedIndex = 0;
+    this.select.elt.dispatchEvent(new Event('change'));
+
   };
 
   this.destroy = function() {
@@ -80,10 +88,15 @@ function Vaccine(){
       return;
     }
 
+    var x = width /2; 
+    var y = height/2;
     //draw the title of the chart
-    /* fill(0)
-    textSize(40)
-    text(this.title, 200, 200); */
+    var title = this.title + this.selectedCountry;
+    strokeWeight(2)
+    textAlign('center', 'center');
+    textSize(24);
+    textStyle(NORMAL)
+    text(title, x, y - 300);
 
     if (!this.relevantRow) {
       console.log('this.relevantRow :', this.relevantRow);
@@ -97,6 +110,8 @@ function Vaccine(){
       this.waffles[i].checkMouse(mouseX,mouseY);
   
     }
+
+
     pop();
   };
 }
